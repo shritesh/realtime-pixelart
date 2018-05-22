b17361458d7f8519174a4ea7d6e953a48633233a
Shritesh Bhattarai <shr@ite.sh>
2018-05-22 00:20:51 -0500

    Read host from environment

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :real_time, RealTimeWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: Map.fetch!(System.get_env(), "HOSTNAME"), port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
